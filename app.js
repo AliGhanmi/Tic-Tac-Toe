80f07135762ca7aefc7b5f0e1e0fe56f47f2e755
AliGhanmi <ghanmiali7@gmail.com>
2021-02-02 13:11:15 +0100

    good

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('running on index html successfully')
+var squares = document.getElementsByClassName('square');
+console.log('squares', squares);
+for(var i = 0; i < squares.length; i++){
+    squares[i].addEventListener('click', myFunction);
+}
+function myFunction(event){
+    event.target.innerHTML ="X"
+    console.log('icons', event.target.innerHTML)
+}
+ 
+
