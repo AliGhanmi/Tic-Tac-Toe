bb25b7287159e0491cb0f660f0c545f69b818db3
AliGhanmi <ghanmiali7@gmail.com>
2021-02-02 14:48:24 +0100

    not yet

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 console.log('running on index html successfully')
 var squares = document.getElementsByClassName('square');
 console.log('squares', squares);
-for(var i = 0; i < squares.length; i++){
+for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', myFunction);
 }
-function myFunction(event){
-    event.target.innerHTML ="X"
-    console.log('icons', event.target.innerHTML)
+var counter = 1
+function myFunction(event) {
+    if (event.target.innerHTML.length === 0) {
+        if (counter % 2 !== 0) {
+            event.target.innerHTML = "X"
+            console.log('icons', event.target.innerHTML)
+            counter++
+            console.log('counter', counter)
+        }
+        else {
+            event.target.innerHTML = "O"
+            console.log('icons', event.target.innerHTML)
+            counter++
+            console.log('counter1', counter)
+        }
+    } else {
+        alert('icon is clicked try another one')
+    }
+
 }
- 
+
 
